453c392b66a787c1ae992231c72369e467febf20
ipeinado <i.peinado.martinez@gmail.com>
2019-02-15 15:35:28 +0100

    Test image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Img from "gatsby-image"
+
+export default ({ data }) => (
+	<div>
+    <h1>Hello gatsby Image!</h1>
+    <Img fluid={ data.file.childImageSharp.fluid } />
+	</div>
+)
+
+export const query = graphql`
+  query {
+    file(relativePath: {eq: "hero_background.jpg"}) {
+      childImageSharp {
+        fluid(quality: 80, maxWidth: 1200) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
