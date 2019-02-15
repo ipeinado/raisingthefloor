2350a13d7e6073e73bec7d42de844cb9d604c92e
ipeinado <i.peinado.martinez@gmail.com>
2019-02-15 15:45:10 +0100

    added more images·

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,32 @@ import Img from "gatsby-image"
 export default ({ data }) => (
 	<div>
     <h1>Hello gatsby Image!</h1>
-    <Img fluid={ data.file.childImageSharp.fluid } />
+    <Img fluid={ data.heroBackground.childImageSharp.fluid } />
+    <Img fluid={ data.kidsTablet.childImageSharp.fluid } />
+    <Img fluid={ data.gpiiBackground.childImageSharp.fluid } />
 	</div>
 )
 
 export const query = graphql`
   query {
-    file(relativePath: {eq: "hero_background.jpg"}) {
+    heroBackground: file(relativePath: {eq: "hero_background.jpg"}) {
       childImageSharp {
-        fluid(quality: 80, maxWidth: 1200) {
-          ...GatsbyImageSharpFluid
+        fluid(quality: 80, maxWidth: 2000) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+    kidsTablet: file(relativePath: {eq: "kids_tablet.jpg"}) {
+      childImageSharp {
+        fluid(quality: 80, maxWidth: 420) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+    gpiiBackground: file(relativePath: {eq: "gpii_background.jpg"}) {
+      childImageSharp {
+        fluid(quality: 80, maxWidth: 2000) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
         }
       }
     }
